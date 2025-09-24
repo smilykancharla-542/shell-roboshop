e019e2b41f33feef449ae22da9b42036cc8e92f7
smily kancharla <smilykancharla@gmail.com>
2025-09-24 20:40:38 +0530

    roboshop using shell script few changes

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0494b89cb6d5db382"
+ZONE_ID="Z01261487TFX8V684K99"
+DOMAIN_NAME="saws86s.fun"
 
 
 
@@ -13,7 +15,30 @@ do
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
 done
+
+echo "$instance: $IP"
+
+aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
